ee106b37b661e5c586b469c09352ee6903de9c2b
Rishav Goenka <rishavgoenka@Rishavs-MacBook-Air.local>
2025-02-11 11:27:15 +0530

    StudentAddress.java

---
+++ src/main/java/com/fsp/fsp/StudentAddress.java
@@ -0,0 +1,28 @@
+package com.fsp.fsp;
+
+public class StudentAddress {
+    private String streetNo;
+    private String city;
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public String getStreetNo() {
+        return streetNo;
+    }
+    public void setStreetNo(String streetNo) {
+        this.streetNo = streetNo;
+    }
+    
+}
